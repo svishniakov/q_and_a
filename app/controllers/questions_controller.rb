5cd03b3abbbf7087f6089272dd7f912e15b12ce9
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-06-04 12:20:01 +0300

    new and edit methods and specs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,17 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[show]
+  before_action :set_question, only: %i[show edit]
 
   def index
     @questions = Question.all
   end
 
   def show; end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit; end
 
   private
 
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:questions)).to match_array(questions)
     end
 
-    it { should render_template('index') }
+    it { should render_template :index }
   end
-  
+
   describe 'GET #show' do
     let(:question) { create(:question) }
 
@@ -22,6 +22,27 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
-    it { should render_template('show') }
+    it { should render_template :show }
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'sets new question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it { should render_template :new }
+  end
+
+  describe 'GET #edit' do
+    let(:question) { create(:question) }
+    before { get :edit, params: {id: question} }
+
+    it 'sets requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it { should render_template :edit }
   end
 end

88f3fd074b3da474e14d5ccfbc2b709b9170ba87
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-06-04 07:33:24 +0300

    Cotext added to tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should validate_presence_of :body }
-  it { should belong_to :question }
+  context 'validations' do
+    it { should validate_presence_of :body }
+  end
+
+  context 'relations' do
+    it { should belong_to :question }
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
-  it { should have_many :answers }
+  context 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+  end
+
+  context 'relations' do
+    it { should have_many :answers }
+  end
 end

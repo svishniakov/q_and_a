8b0c10538db3b78f0660f83c1065118cb2fc5a9d
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-06-06 23:54:09 +0300

    Devise pages styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).on('click', '.notification > button.delete', function() {
+    $(this).parent().addClass('is-hidden');
+    return false;
+});

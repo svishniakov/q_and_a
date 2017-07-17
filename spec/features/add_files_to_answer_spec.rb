9316eb9bcb99919aa9e0f2dc383f65651d8f5994
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-07-17 19:13:09 +0200

    Multiple files attachments specs

--- spec/features/add_files_to_answer_spec.rb
+++ spec/features/add_files_to_answer_spec.rb
@@ -10,14 +10,21 @@ feature 'Possibility to add an attachment to an answer' do
       visit question_path(question)
     end
 
-    scenario 'User can add an attachment during answer creation', js: true do
-      fill_in 'answer_body', with: 'My answer with attachment!'
+    scenario 'User can add attachments during answer creation', js: true do
+      fill_in 'answer_body', with: 'My answer with attachments!'
+
       attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+      click_on 'Upload file'
+      within page.all('.attachment-fields')[1] do
+        attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      end
+
       click_on 'Post answer'
 
-      expect(current_path).to eq question_path(question)
       within '#answers' do
         expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+        expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
       end
     end
   end
--- spec/features/add_files_to_question_spec.rb
+++ spec/features/add_files_to_question_spec.rb
@@ -14,10 +14,18 @@ feature 'Possibility to add an attachment to a question' do
     scenario 'User can add an attachment during question creation', js: true do
       fill_in 'Title', with: question.title
       fill_in 'Body', with: question.body
+
       attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      
+      click_on 'Upload file'
+      within page.all('.attachment-fields')[1] do
+        attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      end
+
       click_on 'Save question'
 
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
     end
   end
 end

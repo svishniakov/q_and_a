8802d4c5c1df774e55335f285b2e9993db6ec10f
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-07-18 15:34:45 +0200

    Attachable re-factoring using concerns

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,7 @@
 class Question < ApplicationRecord
+  include Attachable
   has_many :answers, dependent: :destroy
-  has_many :attachments, as: :attachable
   belongs_to :user
 
   validates :title, :body, presence: true
-
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 end
